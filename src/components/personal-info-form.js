35fe8fad6afbe4e1d1cee7435b5555e4455895cb
tylertran349 <tylertran349@gmail.com>
2023-01-20 21:49:13 -0800

    Clean up event handler

--- src/components/personal-info-form.js
+++ src/components/personal-info-form.js
@@ -11,35 +11,14 @@ class PersonalInfo extends Component {
         email: '',
         phoneNumber: ''
       };
-  
-      this.changeFirstName = this.changeFirstName.bind(this);
-      this.changeLastName = this.changeLastName.bind(this);
-      this.changeEmail = this.changeEmail.bind(this);
-      this.changePhoneNumber = this.changePhoneNumber.bind(this);
-    }
-  
-    changeFirstName(e) {
-      e.preventDefault();
-      const { firstName } = this.state; // Destructure state
-      this.setState({ firstName: e.target.value });
-    }
-  
-    changeLastName(e) {
-      e.preventDefault();
-      const { lastName } = this.state; // Destructure state
-      this.setState({ lastName: e.target.value });
+      
+      this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    changeEmail(e) {
-        e.preventDefault();
-        const { email } = this.state; // Destructure state
-        this.setState({ email: e.target.value });
-    }
-
-    changePhoneNumber(e) {
-        e.preventDefault();
-        const { phoneNumber } = this.state; // Destructure state
-        this.setState({ phoneNumber: e.target.value });
+    handleInputChange = (e) => {
+      this.setState({
+        [e.target.name]: e.target.value
+      })
     }
   
     render() {
@@ -47,10 +26,10 @@ class PersonalInfo extends Component {
       return <div id="personal-info-form">
         <form>
           <span id="form-section-title">Personal Information</span>
-          <input type="text" placeholder="First name" onChange={this.changeFirstName}></input>
-          <input type="text" placeholder="Last name" onChange={this.changeLastName}></input>
-          <input type="text" placeholder="Email" onChange={this.changeEmail}></input>
-          <input type="text" placeholder="Phone number" onChange={this.changePhoneNumber}></input>
+          <input type="text" name="firstName" placeholder="First name" onChange={this.handleInputChange}></input>
+          <input type="text" name="lastName" placeholder="Last name" onChange={this.handleInputChange}></input>
+          <input type="text" name="email" placeholder="Email" onChange={this.handleInputChange}></input>
+          <input type="text" name="phoneNumber" placeholder="Phone number" onChange={this.handleInputChange}></input>
         </form>
         <PersonalInfoGenerator firstName={firstName} lastName={lastName} email={email} phoneNumber={phoneNumber} />
       </div>;
