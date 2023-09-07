f609d3abf06a3a16f744915d29ba59ce76dbaed5
tylertran349 <tylertran349@gmail.com>
2023-09-07 15:18:26 -0700

    Change form field text

--- src/components/personal-info-form.js
+++ src/components/personal-info-form.js
@@ -18,6 +18,7 @@ export class PersonalInfoForm extends Component {
           <input type="text" name="lastName" placeholder="Last name" value={this.props.lastName} onChange={this.handleInputChange}></input>
           <input type="text" name="email" placeholder="Email"  value={this.props.email} onChange={this.handleInputChange}></input>
           <input type="text" name="phoneNumber" placeholder="Phone number" value={this.props.phoneNumber} onChange={this.handleInputChange}></input>
+          <input type="text" name="website" placeholder="LinkedIn/GitHub/portfolio/other URL" value={this.props.website} onChange={this.handleInputChange}></input>
         </form>
     );
   }
