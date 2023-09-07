b914962354db1c98cf57f4fa61741eafc9fcc88c
tylertran349 <tylertran349@gmail.com>
2023-09-07 16:26:48 -0700

    Add location form field

--- src/components/personal-info-form.js
+++ src/components/personal-info-form.js
@@ -19,6 +19,7 @@ export class PersonalInfoForm extends Component {
           <input type="text" name="email" placeholder="Email"  value={this.props.email} onChange={this.handleInputChange}></input>
           <input type="text" name="phoneNumber" placeholder="Phone number" value={this.props.phoneNumber} onChange={this.handleInputChange}></input>
           <input type="text" name="website" placeholder="LinkedIn/GitHub/portfolio/other URL" value={this.props.website} onChange={this.handleInputChange}></input>
+          <input type="text" name="location" placeholder="Location" value={this.props.location} onChange={this.handleInputChange}></input>
         </form>
     );
   }
